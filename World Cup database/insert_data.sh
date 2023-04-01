d617493a41c81b52de25d13fd0c29e8b343332c3
artoma <artoma@gmail.com>
2023-04-01 14:50:12 +0300

    Update inser_data.sh

--- World Cup database/insert_data.sh
+++ World Cup database/insert_data.sh
@@ -8,7 +8,7 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
-#echo $($PSQL "TRUNCATE TABLE games, teams")
+echo $($PSQL "TRUNCATE TABLE games, teams")
 
 declare -A UNIQUE_TEAMS
 while IFS=, read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
@@ -32,11 +32,34 @@ do
   query+=", ('${UNIQUE_TEAMS[$i]}')"
   fi
 done
-#echo $($PSQL "$query")
+echo $($PSQL "$query")
 # END insert items - create and execute query
-echo $($PSQL "select * from teams")
 
-mapfile result < <($PSQL "SELECT * from teams;")
-for row in "${result[@]}";do
-    echo team_id:  ${row%$'\t'*}  team_name: ${row#*$'\t'}
-done
+# Get all temas with ids
+resultAllTeams=$($PSQL "SELECT * from teams")
+declare -A TEAMS_ARRAY=()
+while IFS='|' read -r column1 column2; do
+  TEAMS_ARRAY["$column2"]="$column1"
+done <<< "$resultAllTeams"
+# END Get all temas with ids
+
+# insert gameg
+declare insertQuery="insert into games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) values"
+j=0
+while IFS=, read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $WINNER != "winner" && $OPPONENT != "opponent" ]]
+  then
+    WINNER_ID=${TEAMS_ARRAY[$WINNER]}
+    OPPONENT_ID=${TEAMS_ARRAY[$OPPONENT]}
+    if [[ $j == 0 ]]
+    then
+     insertQuery+="($YEAR, '$ROUND',  $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)"
+    else
+      insertQuery+=", ($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)"
+    fi
+    j=$((j+1))
+  fi  
+done < games.csv
+echo $($PSQL "$insertQuery")
+# END insert games
