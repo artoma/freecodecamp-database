8ae2b1c6d31a375c1c1d44266ab0a718caa93b9c
artoma <artoma@gmail.com>
2023-04-05 12:07:27 +0300

    Update sqlon.sh

--- Salon/salon.sh
+++ Salon/salon.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 
-
-MAIN_MENU(){
+SELECT_SERVICE(){
+  echo -e "\n$1"
   SERVICES_LIST=$($PSQL "select service_id, name from services")
   echo -e "\nSelect your service:"
   echo "$SERVICES_LIST" | while read SERVICE_ID BAR NAME
@@ -10,10 +10,17 @@ MAIN_MENU(){
     echo "$SERVICE_ID) $NAME"
   done
   read SELECTED_SERVICE_ID
+
+  if [[ ! $SELECTED_SERVICE_ID =~ ^[0-9]+$  ]]
+  then
+    SELECT_SERVICE "erro1"
+  else
+    SERVICE=$($PSQL "select * from services where service_id=$SELECTED_SERVICE_ID")
+    if [[ -z $SERVICE ]]
+    then
+      SELECT_SERVICE "error 2"
+    fi
+  fi
+  echo $SELECTED_SERVICE_ID
 }
-MAIN_MENU
-if [[ -z $SELECTED_SERVICE_ID ]]
-then
-  MAIN_MENU
-fi
-echo $SELECTED_SERVICE_ID
+SELECT_SERVICE
