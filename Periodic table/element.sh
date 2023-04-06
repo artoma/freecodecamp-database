ab2bb0b3c05bca87e541b23fd106669ab5907eca
artoma <artoma@gmail.com>
2023-04-06 14:38:08 +0300

    add: sql file

--- Periodic table/element.sh
+++ Periodic table/element.sh
@@ -6,20 +6,18 @@ then
   echo "Please provide an element as an argument."
   exit
 fi
-
 if [[ ! $1 =~ ^[0-9]+$  ]]
 then
   atomic_number=null
 else
   atomic_number=$1  
 fi
-
-RESULT=$($PSQL "select * from elements full join properties using(atomic_number) where atomic_number = $atomic_number  or name = '$1' or symbol = '$1'")
+RESULT=$($PSQL "select * from elements full join properties using(atomic_number) full join types using(type_id) where atomic_number = $atomic_number  or name = '$1' or symbol = '$1'")
 if [[ -z $RESULT ]]
 then
   echo "I could not find that element in the database."
 else
-  echo "$RESULT" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT_CELSIUS BAR BOILING_POINT_CELSIUS BAR TYPE_ID
+  echo "$RESULT" | while read TYPE_ID BAR ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR ATOMIC_MASS BAR MELTING_POINT_CELSIUS BAR BOILING_POINT_CELSIUS BAR TYPE
   do
     echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
   done
