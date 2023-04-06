f46b7688908262a2f497e3d9c00aff71e4cc9b47
artoma <artoma@gmail.com>
2023-04-06 14:08:23 +0300

    Create element.sh

---
+++ Periodic table/element.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
+if [[ ! $1 =~ ^[0-9]+$  ]]
+then
+  atomic_number=null
+else
+  atomic_number=$1  
+fi
+
+RESULT=$($PSQL "select * from elements full join properties using(atomic_number) where atomic_number = $atomic_number  or name = '$1' or symbol = '$1'")
+if [[ -z $RESULT ]]
+then
+  echo "I could not find that element in the database."
+else
+  echo "$RESULT" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT_CELSIUS BAR BOILING_POINT_CELSIUS BAR TYPE_ID
+  do
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+  done
+fi
+exit
